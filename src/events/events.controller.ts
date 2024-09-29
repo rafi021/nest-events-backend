230bbf3f0f6f13500450971663597dbf814215d0
rafi021 <mahmud.ibrahim021@gmail.com>
2024-09-29 19:07:40 +0600

    responses

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Delete, Param, Body } from '@nestjs/common';
 
 @Controller('events')
 export class EventsController {
   @Get()
   findAll() {
-    return 'This action returns all events.';
+    return [
+      { id: 1, name: 'event 1' },
+      { id: 2, name: 'event 2' },
+      { id: 3, name: 'event 3' },
+    ];
   }
 
   @Get(':id')
   findOne(@Param('id') id) {
-    return id;
+    return { id: 1, name: 'event 1' };
   }
 
   @Post()
